1c4aeac3b7190e8bfdfdfc54325e6965f0a85bda
Daniel Jiménez <daniel.jimenez@scitum.com.mx>
2015-08-20 15:37:22 -0500

    fix nested transactions error

--- grails-app/controllers/mx/com/scitum/PropiedadController.groovy
+++ grails-app/controllers/mx/com/scitum/PropiedadController.groovy
@@ -10,7 +10,7 @@ class PropiedadController {
 
     def unmarshallerService
 
-    static allowedMethods = [save: ["POST", 'OPTIONS'], update: ["PUT", 'OPTIONS'], delete: ["DELETE", 'OPTIONS']]
+    static allowedMethods = [save: ["POST"], update: ["PUT"], delete: ["DELETE"]]
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
@@ -25,11 +25,109 @@ class PropiedadController {
         respond new Propiedad(params)
     }
 
-    @Transactional
+//    @Transactional
     def save() {
-        if(request.method == 'OPTIONS') {respond OK; return;}
+
+
+//        def technologyProps = {
+//            def lista = [
+//                    new Propiedad(nombre: 'cantidad', descripcion: 'cantidad', tipo: 'Integer'),
+//                    new Propiedad(nombre: 'volumetría', descripcion: 'volumetría')
+//            ]
+//            return lista
+//        }
+//        def technologyConcepts = {
+//            def lista = [
+//                    new Concepto(nombre: 'Firewall/NAT', descripcion: 'Firewall/NAT', costo: 23, multiple: true),
+//                    new Concepto(nombre: 'VPN IPSEC', descripcion: 'VPN IPSEC', costo: 2, multiple: true),
+//                    new Concepto(nombre: 'VPN SSL', descripcion: 'VPN SSL', costo: 3, multiple: true),
+//                    new Concepto(nombre: 'IPS', descripcion: 'IPS', costo: 3, multiple: true),
+//                    new Concepto(nombre: 'Application control', descripcion: 'Application control', costo: 3, multiple: true),
+//                    new Concepto(nombre: 'URL filtering', descripcion: 'URL filtering', costo: 3, multiple: true),
+//                    new Concepto(nombre: 'Antivirus', descripcion: 'Antivirus', costo: 123, multiple: true),
+//                    new Concepto(nombre: 'HA', descripcion: 'HA', costo:  23, multiple: true)
+//            ]
+//            return lista
+//        }
+//
+//        ConceptoEspecial conceptoEspecial = new ConceptoEspecial(descripcion: 'Firewall', nombre: 'firewall')
+//        conceptoEspecial.setNombre(conceptoEspecial.descripcion)
+//        conceptoEspecial.setCustomId(conceptoEspecial.descripcion)
+//        arrProp = technologyProps()
+//        arrProp.each {
+//            it.setNombre(it.descripcion)
+//            it.setCustomId(conceptoEspecial.nombre+'_'+it.descripcion)
+//            conceptoEspecial.addToPropiedades(it)
+//        }
+//        arrCon = technologyConcepts()
+//        arrCon.each {
+//            it.setNombre(it.descripcion)
+//            it.setCustomId(conceptoEspecial.nombre+'_'+it.descripcion)
+//            conceptoEspecial.addToConceptosE(it)
+//        }
+//        category.addToComponentes(conceptoEspecial)
+//
+//        conceptoEspecial = new ConceptoEspecial(descripcion: 'IPS', nombre: 'ips')
+//        conceptoEspecial.setNombre(conceptoEspecial.descripcion)
+//        conceptoEspecial.setCustomId(conceptoEspecial.descripcion)
+//        arrProp = technologyProps()
+//        arrProp.each {
+//            it.setNombre(it.descripcion)
+//            it.setCustomId(conceptoEspecial.nombre+'_'+it.descripcion)
+//            conceptoEspecial.addToPropiedades(it)
+//        }
+//        arrCon = technologyConcepts()
+//        arrCon.each {
+//            it.setNombre(it.descripcion)
+//            it.setCustomId(conceptoEspecial.nombre+'_'+it.descripcion)
+//            conceptoEspecial.addToConceptosE(it)
+//        }
+//        category.addToComponentes(conceptoEspecial)
+//
+//        conceptoEspecial = new ConceptoEspecial(descripcion: 'Filtrado web', nombre: 'filtrado web')
+//        conceptoEspecial.setNombre(conceptoEspecial.descripcion)
+//        conceptoEspecial.setCustomId(conceptoEspecial.descripcion)
+//        arrProp = technologyProps()
+//        arrProp.each {
+//            it.setNombre(it.descripcion)
+//            it.setCustomId(conceptoEspecial.nombre+'_'+it.descripcion)
+//            conceptoEspecial.addToPropiedades(it)
+//        }
+//        arrCon = technologyConcepts()
+//        arrCon.each {
+//            it.setNombre(it.descripcion)
+//            it.setCustomId(conceptoEspecial.nombre+'_'+it.descripcion)
+//            conceptoEspecial.addToConceptosE(it)
+//        }
+//        category.addToComponentes(conceptoEspecial)
+//
+//        conceptoEspecial = new ConceptoEspecial(descripcion: 'Antispam', nombre: 'antispam')
+//        conceptoEspecial.setNombre(conceptoEspecial.descripcion)
+//        conceptoEspecial.setCustomId(conceptoEspecial.descripcion)
+//        arrProp = technologyProps()
+//        arrProp.each {
+//            it.setNombre(it.descripcion)
+//            it.setCustomId(conceptoEspecial.nombre+'_'+it.descripcion)
+//            conceptoEspecial.addToPropiedades(it)
+//        }
+//        arrCon = technologyConcepts()
+//        arrCon.each {
+//            it.setNombre(it.descripcion)
+//            it.setCustomId(conceptoEspecial.nombre+'_'+it.descripcion)
+//            conceptoEspecial.addToConceptosE(it)
+//        }
+//        category.addToComponentes(conceptoEspecial)
+//
+//        category.save(flush: true, failOnError: true)
+
+
+
+
         Propiedad propiedadInstance = new Propiedad()
-        bindData(propiedadInstance, request.JSON, [include: ['nombre', 'descripcion', 'tipo']])
+        propiedadInstance.setNombre(request.JSON.nombre)
+        propiedadInstance.setDescripcion(request.JSON.descripcion)
+        propiedadInstance.setTipo(request.JSON.tipo)
+//        bindData(propiedadInstance, request.JSON, [include: ['nombre', 'descripcion', 'tipo']])
 
         def parentJSON = request.JSON.parent
         ConceptoEspecial parent = parentJSON?.id?
@@ -42,11 +140,15 @@ class PropiedadController {
         }
 
         propiedadInstance.setCustomId(parent.customId+'_'+propiedadInstance.nombre)
-        propiedadInstance.propietario = parent
+//        propiedadInstance.propietario = parent
+
+//        propiedadInstance.validate()
+//        propiedadInstance.save(flush: true)
 
         parent.addToPropiedades(propiedadInstance)
 
-        propiedadInstance.validate()
+//        propiedadInstance.validate()
+//        propiedadInstance.save(flush: true)
         if (propiedadInstance.hasErrors()) {
             respond propiedadInstance.errors, view:'create'
             return
@@ -71,7 +173,7 @@ class PropiedadController {
 
     @Transactional
     def update(Propiedad propiedadInstance) {
-        if(request.method == 'OPTIONS') {respond OK; return;}
+//        if(request.method == 'OPTIONS') {respond OK; return;}
         if (propiedadInstance == null) {
             notFound()
             return
@@ -93,9 +195,9 @@ class PropiedadController {
         }
     }
 
-    @Transactional
+//    @Transactional
     def delete() {
-        if(request.method == 'OPTIONS') {respond OK; return;}
+//        if(request.method == 'OPTIONS') {respond OK; return;}
         Propiedad propiedadInstance = request.JSON.id? Propiedad.get(request.JSON.id): Propiedad
                 .findByCustomId(request.JSON.customId)
 
@@ -104,7 +206,7 @@ class PropiedadController {
             return
         }
 
-        ConceptoEspecial parent = propiedadInstance.propietario
+        ConceptoEspecial parent = Item.get(propiedadInstance.propietario.id)
         unmarshallerService.removeDependency(parent, propiedadInstance)
 
 
